ecf22e51d67720a25de27438b648a99faa4aff85
Conner Petzold <cpetzold@gmail.com>
2017-10-14 22:42:50 -0700

    Init

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float maxSpeed = 10f;
+	private Rigidbody2D rigidBody2D;
+
+	void Start () {
+		rigidBody2D = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate () {
+		float move = Input.GetAxis ("Horizontal");
+		rigidBody2D.velocity = new Vector2 (move * maxSpeed, rigidBody2D.velocity.y);
+	}
+}
