fa81dc52c5d21643f7f19182da6526060f09e5ae
Conner Petzold <cpetzold@gmail.com>
2018-01-14 16:10:43 -0800

    More stuff

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,18 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+	public string jumpButton;
+	public string horizontalAxis;
 
 	public float acceleration = 10f;
+	public float maxSpeed = 20f;
+	public float jumpForce = 20f;
 
+	private bool jump = false;
 	private Rigidbody2D rigidBody2D;
 
 	void Start () {
 		rigidBody2D = GetComponent<Rigidbody2D> ();
 	}
 
-	void FixedUpdate () {
-		float move = Input.GetAxis ("Horizontal");
+	void Update() {
+		if (Input.GetButtonDown (jumpButton)) {
+			jump = true;
+		}
+	}
 
-		rigidBody2D.AddForce (new Vector2 (move * acceleration, 0));
+	void FixedUpdate () {
+		float move = Input.GetAxis (horizontalAxis);
+
+		rigidBody2D.AddForce (Vector2.right * move * acceleration);
+		if (Mathf.Abs(rigidBody2D.velocity.x) > maxSpeed) {
+			rigidBody2D.velocity = new Vector2 (Mathf.Clamp (rigidBody2D.velocity.x, -maxSpeed, maxSpeed), rigidBody2D.velocity.y);
+		}
+
+		if (jump) {
+			rigidBody2D.AddForce (Vector2.up * jumpForce);
+			jump = false;
+		}
 	}
 }
